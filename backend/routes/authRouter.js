f7d3cad9be0dc086542f2c3f94e8938035f7153b
ray <raymond.shan@datatech.ai>
2020-02-08 19:27:25 +0800

    Backend - Enhance middleware

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {isCorrectUser, requiredLogin} = require('../libs/jwt');
 const router = express.Router();
+const db = require('../db')
 
 router.use((req, res, next) => {
 	let token = req.header('x-access-token');
@@ -9,11 +10,20 @@ router.use((req, res, next) => {
 		// Check whether user logined or not
 		const result = requiredLogin(token);
 		if(!!result){
-			if(isCorrectUser(authKey, result.userId)) next();
-			else res.status(401).json({status : 401, message : 'You are not the correct user'});
-		}else res.status(401).json({status:401, message : 'Login fail'});
+			if(isCorrectUser(authKey, result.userId)) {
+				db.query("SELECT * FROM user where userId = ?", [result.userId], function(err, rows){
+					if(rows && rows.length>0) {
+						if(rows[0]['token'] === token) next();
+						else res.status(401).json({success : false, message : 'Please Login Again'});
+					}else{
+						res.status(401).json({success : false, message : 'Unexpected server error'});
+					}
+				})
+			}
+			else res.status(401).json({success : false, message : 'You are not the correct user'});
+		}else res.status(401).json({success : false, message : 'Login fail'});
 	}else{
-		res.status(401).json({status:401, message : 'Please login first'});
+		res.status(401).json({sucess : false, message : 'Please login first'});
 	}
 });
 
